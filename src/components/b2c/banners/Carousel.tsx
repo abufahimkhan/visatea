bb36898dc645d762446e4f7e00c23683a08b0135
abufahimkhan <abufahimkhan1@gmail.com>
2024-10-11 00:51:55 +0600

    First commit

---
+++ src/components/b2c/banners/Carousel.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Images } from "@/components/utils/image";
+import { nanoid } from "nanoid";
+import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 2000,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const banners = [{ logo: Images.banner1 }, { logo: Images.banner2 }];
+
+  return (
+    <div className="slider-container overflow-hidden">
+      <Slider {...settings}>
+        {banners.map((banner) => (
+          <div key={nanoid()} className="banner-wrapper">
+            <Image
+              src={banner.logo}
+              alt="banners"
+              layout="responsive" // Ensures the image is responsive
+              width={1600} // Adjust based on your banner width
+              height={600} // Adjust based on your banner height
+              objectFit="cover" // Makes the image cover the container, maintaining aspect ratio
+              quality={100} // Ensures the image is high quality
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/utils/image/index.ts
@@ -0,0 +1,8 @@
+import logo from "@/app/assets/images/header/logo.png";
+import banner1 from "@/app/assets/images/banner/b1.jpg";
+import banner2 from "@/app/assets/images/banner/b2.jpg";
+export const Images = {
+  logo,
+  banner1,
+  banner2,
+};
