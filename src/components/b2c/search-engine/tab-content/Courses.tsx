9854827026e9d2e05b625fd5924c551e5e144eee
abufahimkhan <abufahimkhan1@gmail.com>
2024-10-16 12:12:34 +0600

    Code edited

--- src/components/b2c/search-engine/tab-content/Courses.tsx
+++ src/components/b2c/search-engine/tab-content/Courses.tsx
@@ -8,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2 } from "lucide-react";
