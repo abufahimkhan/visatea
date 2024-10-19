8930f92a0017a7fabd5fa34880c7ad58190b7a9a
abufahimkhan <abufahimkhan1@gmail.com>
2024-10-19 20:06:19 +0600

    Destination added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Carousel from "@/components/b2c/banners/Carousel";
 import Divider from "@/components/b2c/common/Divider";
+import TitleAnimated from "@/components/b2c/common/TitleAnimated";
 import ContactUs from "@/components/b2c/contact-us/ContactUs";
+import Destination from "@/components/b2c/destination/Destination";
 import Footer from "@/components/b2c/footer/Footer";
 import { SearchEngine } from "@/components/b2c/search-engine/SearchEngine";
 import Services from "@/components/b2c/services/Services";
@@ -9,11 +11,19 @@ export default function Home() {
   return (
     <div className="">
       <Carousel />
-      <Divider title="Visa Tea is Here To help You" />
+      <TitleAnimated
+        title="Search For Courses"
+        title2="Search For Scholarships"
+        title3="Search For Universities"
+        title4="Search For Events"
+        title5="Search For Road Map"
+      />
       <SearchEngine />
+      <Divider />
       <Services />
       <Divider title="Contact for your Scope" />
       <ContactUs />
+      <Destination />
       <Footer />
     </div>
   );
