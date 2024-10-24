0e42994f611d0d2447dbedc40aeb3e385e72943f
abufahimkhan <abufahimkhan1@gmail.com>
2024-10-24 16:53:36 +0600

    Hover effect for contents pn card added

---
+++ src/components/utils/data/DestinationContents.ts
@@ -0,0 +1,6 @@
+export const DestinationContents = [
+  {
+    australia:
+      "A photograph of the Sydney Opera House along the water's edge AustraliaStudy in AustraliaUncover a world of opportunities in the land down under! With over 1,100 institutions, and 22,000 courses to choose from, Australia offers a world-class education and an outstanding quality of life.",
+  },
+];
